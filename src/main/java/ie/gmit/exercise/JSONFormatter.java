af822ada5af705258174753ced81170299de0abf
JOSUA BRINK - STUDENT <G00362643@gmit.ie>
2022-03-30 09:50:20 +0100

    RefactoredCode

--- src/main/java/ie/gmit/exercise/JSONFormatter.java
+++ src/main/java/ie/gmit/exercise/JSONFormatter.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
-public class JSONFormatter implements Formatter{
+public class JSONFormatter implements Formatter {
 
 	public String format(Message message) throws FormatException {
 		ObjectMapper mapper = new ObjectMapper();
